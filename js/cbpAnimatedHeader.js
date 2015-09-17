954d3510dd0ba727b62cb99040a06ca50a4ed08f
micahblu <micahblu@gmail.com>
2015-09-16 19:46:20 -0700

    Modularized

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,41 +4,57 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var cbpAnimatedHeader = (function() {
 
-	var docElem = document.documentElement,
-		header = document.querySelector( '.cbp-af-header' ),
-		didScroll = false,
-		changeHeaderOn = 300;
-
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
+(function(global, factory) {
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( factory() );
+	} else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = factory();
+	} else {
+		// browser global
+		global.cbpAnimatedHeader = factory();
 	}
+})(window, function() {
 
-	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'cbp-af-header-shrink' );
-		}
-		else {
-			classie.remove( header, 'cbp-af-header-shrink' );
-		}
-		didScroll = false;
-	}
+	 var docElem = document.documentElement,
+ 			header = {},
+ 			didScroll = false,
+ 			changeHeaderOn = 300;
 
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
+ 	function init() {
 
-	init();
+ 		header = document.querySelector( '.cbp-af-header' )
 
-})();
+ 		window.addEventListener( 'scroll', function( event ) {
+ 			if( !didScroll ) {
+ 				didScroll = true;
+ 				setTimeout( scrollPage, 250 );
+ 			}
+ 		}, false );
+ 	}
+
+ 	function scrollPage() {
+ 		var sy = scrollY();
+ 		if ( sy >= changeHeaderOn ) {
+ 			classie.add( header, 'cbp-af-header-shrink' );
+ 		}
+ 		else {
+ 			classie.remove( header, 'cbp-af-header-shrink' );
+ 		}
+ 		didScroll = false;
+ 	}
+
+ 	function scrollY() {
+ 		return window.pageYOffset || docElem.scrollTop;
+ 	}
+
+ 	return {
+		init: init
+ 	};
+});
